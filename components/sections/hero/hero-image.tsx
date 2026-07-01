e90fcfc94a31b79bce76a8a2c876da06e7b0cafa
farzane <farzanekazemi8517@gmail.com>
2026-07-01 18:45:37 +0330

    ref: hero image

--- components/sections/hero/hero-image.tsx
+++ components/sections/hero/hero-image.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
-import dashboard from "@/public/images/hero.webp";
+import hero from "@/public/images/hero.webp";
 export default function HeroImage() {
   return (
-    <Image src={dashboard} priority alt="Dashboard" width={1200} height={700} />
+    <Image
+      src={hero}
+      priority
+      alt="Screenshot of appointments in admin dashboard"
+      className="w-full h-auto"
+    />
   );
 }
