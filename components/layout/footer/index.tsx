73d149418b642643551fa26bf219671494bed357
farzane <farzanekazemi8517@gmail.com>
2026-07-18 22:48:32 +0330

    ref: social links and icons

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -1,74 +1,74 @@
 import Logo from "@/components/shared/logo";
 import Link from "next/link";
 import Facebook from "@/components/ui/icons/facebook.svg";
-import Twitter from "@/components/ui/icons/twitter.svg";
+import Threads from "@/components/ui/icons/threads-app.svg";
 import Instagram from "@/components/ui/icons/instagram.svg";
-import LinkedIn from "@/components/ui/icons/linkedin.svg";
+import Email from "@/components/ui/icons/email.svg";
 import Language from "./language";
 import { getTranslations } from "next-intl/server";
 
 const socials = [
   {
-    icon: <Facebook className="w-8 h-8" />,
-    href: "#",
+    icon: <Facebook className="h-8 w-8" />,
+    href: process.env.NEXT_PUBLIC_FACEBOOK_URL,
     label: "Facebook",
   },
   {
-    icon: <Instagram className="w-8 h-8" />,
-    href: "#",
+    icon: <Instagram className="h-8 w-8" />,
+    href: process.env.NEXT_PUBLIC_INSTAGRAM_URL,
     label: "Instagram",
   },
   {
-    icon: <Twitter className="w-8 h-8" />,
-    href: "#",
-    label: "Twitter",
+    icon: <Threads className="h-8 w-8 fill-black" />,
+    href: process.env.NEXT_PUBLIC_THREADS_URL,
+    label: "Threads",
   },
   {
-    icon: <LinkedIn className="w-8 h-8" />,
-    href: "#",
-    label: "Linkedin",
+    icon: <Email className="h-8 w-8" />,
+    href: process.env.NEXT_PUBLIC_EMAIL_URL,
+    label: "Email",
   },
 ];
 
 export default async function Footer() {
   const t = await getTranslations("footer");
   return (
-    <footer className="w-full md:flex-row flex-col flex justify-between items-start px-12 py-8 lg:gap-x-12">
-      <div className="flex gap-y-4 flex-col items-start justify-start md:max-w-1/4 w-full">
-        <div className="flex justify-start items-center gap-x-2">
+    <footer className="flex w-full flex-col items-start justify-between px-12 py-8 md:flex-row lg:gap-x-12">
+      <div className="flex w-full flex-col items-start justify-start gap-y-4 md:max-w-1/4">
+        <div className="flex items-center justify-start gap-x-2">
           <Logo />
-          <p className="font-extrabold text-xl">{t("appname")}</p>
+          <p className="text-xl font-extrabold">{t("appname")}</p>
         </div>
-        <p className="text-sm text-slate-900 font-medium">{t("description")}</p>
-        <div className="social-links flex justify-between items-center w-full">
+        <p className="text-sm font-medium text-slate-900">{t("description")}</p>
+        <div className="social-links flex w-full items-center justify-between">
           {socials.map((item) => (
-            <Link key={item.label} href={item.href} aria-label={item.label}>
+            <Link key={item.label} href={item.href || "#"} aria-label={item.label}>
               {item.icon}
             </Link>
           ))}
         </div>
       </div>
       {/* placeholder */}
-      <div className="hidden md:flex flex-col items-start justify-start gap-y-2 text-md text-slate-900 font-semibold">
+      <div className="text-md hidden flex-col items-start justify-start gap-y-2 font-semibold text-slate-900 md:flex">
         <Link href="/">{t("product")}</Link>
         <Link href="/">{t("links.pricing")}</Link>
         <Link href="/">{t("links.features")}</Link>
         <Link href="/">{t("links.faq")}</Link>
       </div>
-      <div className="hidden md:flex flex-col items-start justify-start gap-y-2 text-md text-slate-900 font-semibold">
+      <div className="text-md hidden flex-col items-start justify-start gap-y-2 font-semibold text-slate-900 md:flex">
         <Link href="/">{t("product")}</Link>
         <Link href="/">{t("links.pricing")}</Link>
         <Link href="/">{t("links.features")}</Link>
         <Link href="/">{t("links.faq")}</Link>
       </div>
-      <div className="hidden md:flex flex-col items-start justify-start gap-y-2 text-md text-slate-900 font-semibold">
+      <div className="text-md hidden flex-col items-start justify-start gap-y-2 font-semibold text-slate-900 md:flex">
         <Link href="/">{t("product")}</Link>
         <Link href="/">{t("links.pricing")}</Link>
         <Link href="/">{t("links.features")}</Link>
         <Link href="/">{t("links.faq")}</Link>
       </div>
-      <div className="w-full hidden max-w-1/5 md:flex flex-col items-center justify-start gap-y-2 text-md text-slate-900 font-semibold">
-        <p className="text-lg text-slate-900 font-bold">{t("language")}</p>
+      <div className="text-md hidden w-full max-w-1/5 flex-col items-center justify-start gap-y-2 font-semibold text-slate-900 md:flex">
+        <p className="text-lg font-bold text-slate-900">{t("language")}</p>
         <Language />
       </div>
     </footer>
